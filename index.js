590e6d68d6d5a6d5fd241cd98a660d7998ceeb1f
DLF414 <Dmitry220198@gmail.com>
2018-03-27 01:06:18 +0300

    vot2

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-console.log('Package v1.0.1');
-module.exports = (polishNotationString, message) => {
-    let tokens = polishNotationString.split(' ');
-
-    console.log(message);
-    return Calculator.calculate(tokens);
+const operators = {
+    '+': (a, b) => b + a,
+    '-': (a, b) => b - a,
+    '*': (a, b) => b * a,
+    '/': (a, b) => b / a,
 };
 
+const isOperator = (token) => token in operators;
 
-const Calculator = {
-    calculate: (tokens) => {
-        let end = tokens.length,
-            stack = [];
+const isValue = (token) => !isNaN(parseFloat(token)) && isFinite(token);
 
-        for (let i = 0; i < end; i++) {
-            let token = tokens[i];
+module.exports = (expression) => {
+    expression = expression.trim();
 
-            if (Calculator.isOperand(token))
-                stack.push(Number(token));
-            if (Calculator.isOperator(token))
-                stack.push(Calculator.doOperate(token, stack.pop(), stack.pop()))
+    if (expression === '') return null;
+
+    const tokens = expression.split(/\s+/);
+
+    const stack = [];
+    while (tokens.length) {
+        let token = tokens.shift();
+
+        if (isValue(token)) {
+            stack.push(token);
+        } else if (isOperator(token)) {
+            let a = stack.pop();
+            let b = stack.pop();
+
+            if (a === null || b === null) return null;
+
+            stack.push(operators[token](+a, +b));
+        } else {
+            return null;
         }
-
-        return stack.pop();
-    },
-
-    isOperator: token => {
-        return /[-\+\*\/]/.test(token)
-    },
-    isOperand: token => {
-        return /\d+/.test(token)
-    },
-    doOperate: (operator, a, b) => {
-        return Calculator.operations[operator](a, b);
-    },
-    operations: {
-        '+': (a, b) => b + a,
-        '-': (a, b) => b - a,
-        '*': (a, b) => b * a,
-        '/': (a, b) => b / a,
     }
-}
+
+    return stack.length !== 1 ? null : stack.pop();
+};
 
